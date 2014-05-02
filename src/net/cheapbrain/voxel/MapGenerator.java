1d46db5d7c45b21f0a1bd0e1766cad820a2f80c7
cheapbrain <ermarigo96@gmail.com>
2014-05-02 22:42:39 +0200

    new generator

--- src/net/cheapbrain/voxel/MapGenerator.java
+++ src/net/cheapbrain/voxel/MapGenerator.java
@@ -8,6 +8,7 @@ public class MapGenerator {
 		short[][][][][] blocks = new short[wheight][Chunk.SIZE][Chunk.SIZE][Chunk.SIZE][2];
 		for (int x=0;x<16;x++)
 			for (int z=0;z<16;z++) {
+				
 				double hvalue = SimplexNoise.noise((x+cx*Chunk.SIZE)/100d, (z+cz*Chunk.SIZE)/100d, (float)seed);
 				hvalue +=  SimplexNoise.noise((x+cx*Chunk.SIZE)/50d, (z+cz*Chunk.SIZE)/50d, (float)seed)*0.4;
 				hvalue +=  SimplexNoise.noise((x+cx*Chunk.SIZE)/10d, (z+cz*Chunk.SIZE)/10d, (float)seed)*0.05;
@@ -18,13 +19,17 @@ public class MapGenerator {
 				else
 					height = (int)(hvalue*32+128);
 				for (int cy=0;cy<wheight;cy++)
-					for (int y=0;y<16;y++)
-						if (y+cy*Chunk.SIZE==height)
-							blocks[cy][x][y][z][0] = 3;
-						else if (y+cy*Chunk.SIZE<height-2)
-							blocks[cy][x][y][z][0] = 1;
-						else if (y+cy*Chunk.SIZE<height)
-							blocks[cy][x][y][z][0] = 2;
+					for (int y=0;y<16;y++) {
+						double hole = SimplexNoise.noise((x+cx*Chunk.SIZE)/10d, (y+cy*Chunk.SIZE)/10d, (z+cz*Chunk.SIZE)/10d, (float)seed);
+						if (hole>=0) {
+							if (y+cy*Chunk.SIZE==height)
+								blocks[cy][x][y][z][0] = 3;
+							else if (y+cy*Chunk.SIZE<height-2)
+								blocks[cy][x][y][z][0] = 1;
+							else if (y+cy*Chunk.SIZE<height)
+								blocks[cy][x][y][z][0] = 2;
+						}
+					}
 			}
 		Chunk[] chunks = new Chunk[wheight];
 		for (int cy=0;cy<wheight;cy++)
